a2b30632da9af9d87f8675aa506aae644dd50525
Aruldas3211 <71898060+Aruldas3211@users.noreply.github.com>
2021-02-20 13:04:17 +0530

    Update webstagrambot.py

--- webstagrambot.py
+++ webstagrambot.py
@@ -39,8 +39,8 @@ import time
 ##### EDIT THESE BELOW
 
 # your instagram username and password
-username = "username"
-password = "password"
+username = "the_americium_95"
+password = "aruldas123"
 
 #set a sleep timer between each like.  Set value to 0 if you don't want it to sleep at all
 sleeptimer = 5
